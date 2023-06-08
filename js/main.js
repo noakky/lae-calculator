7c0fd27caf4ea7bb0e23f25b4d78d07094feefe3
Vyacheslav Makodin <noakky@gmail.com>
2023-06-08 10:27:15 +0500

    minor fix
    Дробные значения ввода
    Кнопка «Обновить»

--- js/main.js
+++ js/main.js
@@ -1,126 +1,125 @@
-        var form1 = document.getElementById("form1");
-        var form2 = document.getElementById("form2");
-        var form3 = document.getElementById("form3");
-        var form4 = document.getElementById("form4");
-        var result = document.getElementById('conclusion');
-        var result = document.getElementById('content');
-        const surgery = [
-                            'N. Лимфаденэктомия не показана',
-                            'I. Биопсия сторожевого лимфатического узла со срочной гистологией',
-                            'II. Поверхностная тазово-бедренная подвздошная лимфаденэктомия',
-                            'III. Операция Дюкена',
-                            'IV. Тазово-бедренная подвздошная лимфаденэктомия',
-                            'Операция окончена',
-                            ];
-        function submitForm(event) {
-            event.preventDefault();
-            var msptlu = document.getElementById('msptlu-y').checked;
-            var mspblu = document.getElementById('mspblu-y').checked;
-            var depth = document.getElementById('depth').value;
-            var size = document.getElementById('size').value;
-            var dist = document.getElementById('dist').value;
-            form2.style.display = "none";
-            form3.style.display = "none";
-            form4.style.display = "none";
-            conclusion.style.display = "none";
-            result.innerHTML = '';
-            if (msptlu == true) {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML += surgery[4];
-                return;
-            } else if (mspblu == true) {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML += surgery[3];
-                return;
-            }
-            if (size <= '2' && depth <= '1') {
-                conclusion.style.display = "inline-grid";
-                    result.innerHTML += surgery[0];
-                    return;
-            }
-            if ((size > '2' && size < '4') || (size <= '2' && depth >'1')) {
-                if (dist > '2') {
-                    form2.style.display = "block";
-                    let x = document.querySelector('input[name=biopsy]:checked');
-                if (x) {
-                    x.checked = false;
-                }
-                    return;
-                } else {
-                    conclusion.style.display = "inline-grid";
-                    result.innerHTML += surgery[2];
-                    form4.style.display = "block";
-                    let x = document.querySelector('input[name=damage]:checked');
-                if (x) {
-                    x.checked = false;
-                }
-                    return;
-                }
-
-            }
-            if (size > 4) {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML += surgery[2];
-                form4.style.display = "block";
-                let x = document.querySelector('input[name=damage]:checked');
-                if (x) {
-                    x.checked = false;
-                }
-                return;
-            }
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
+var form3 = document.getElementById("form3");
+var form4 = document.getElementById("form4");
+var conclusion = document.getElementById('conclusion');
+var content = document.getElementById('content');
+const surgery = [
+                    'N. Лимфаденэктомия не показана',
+                    'I. Биопсия сторожевого лимфатического узла со срочной гистологией',
+                    'II. Поверхностная тазово-бедренная лимфаденэктомия',
+                    'III. Операция Дюкена',
+                    'IV. Тазово-бедренная подвздошная лимфаденэктомия',
+                    'Операция окончена',
+                    ];
+function submitForm(event) {
+    event.preventDefault();
+    var msptlu = document.getElementById('msptlu-y').checked;
+    var mspblu = document.getElementById('mspblu-y').checked;
+    var depth = document.getElementById('depth').value;
+    var size = document.getElementById('size').value;
+    var dist = document.getElementById('dist').value;
+    form2.style.display = "none";
+    form3.style.display = "none";
+    form4.style.display = "none";
+    conclusion.style.display = "none";
+    content.innerHTML = '';
+    if (msptlu == true) {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML += surgery[4];
+        return;
+    } else if (mspblu == true) {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML += surgery[3];
+        return;
+    }
+    if (size <= '2' && depth <= '1') {
+        conclusion.style.display = "inline-grid";
+            content.innerHTML += surgery[0];
+            return;
+    }
+    if ((size > '2' && size < '4') || (size <= '2' && depth >'1')) {
+        if (dist > '2') {
+            form2.style.display = "block";
+            let x = document.querySelector('input[name=biopsy]:checked');
+        if (x) {
+            x.checked = false;
         }
-        function biopsy(event) {
-            event.preventDefault();
-            var biopsy = document.getElementById('biopsy-y').checked;
-            form3.style.display = "none";
-            form4.style.display = "none";
-            if (biopsy == true) {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML = surgery[1];
-                form3.style.display = "block";
-                let x = document.querySelector('input[name=meta]:checked');
-                if (x) {
-                    x.checked = false;
-                }
-                return;
-            } else {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML = surgery[2];
-                form4.style.display = "block";
-                let x = document.querySelector('input[name=damage]:checked');
-                if (x) {
-                    x.checked = false;
-                }
-                return;
-            }
+            return;
+        } else {
+            conclusion.style.display = "inline-grid";
+            content.innerHTML += surgery[2];
+            form4.style.display = "block";
+            let x = document.querySelector('input[name=damage]:checked');
+        if (x) {
+            x.checked = false;
         }
-        function meta(event) {
-            event.preventDefault();
-            var meta = document.getElementById('meta-y').checked;
-            if (meta == true) {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML = surgery[3];
-                return;
-            } else {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML = surgery[5];
-                return;
-            }
+            return;
         }
-        function damage(event) {    
-            event.preventDefault();
-            var damage = document.getElementById('damage-y').checked;
-            if (damage == true) {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML = surgery[3];
-                return;
-            } else {
-                conclusion.style.display = "inline-grid";
-                result.innerHTML = surgery[5];
-                return;
-            }
+    }
+    if (size > 4) {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML += surgery[2];
+        form4.style.display = "block";
+        let x = document.querySelector('input[name=damage]:checked');
+        if (x) {
+            x.checked = false;
         }
-    form1.addEventListener('submit', submitForm);
-    form2.addEventListener('submit', biopsy);
-    form3.addEventListener('submit', meta);
-    form4.addEventListener('submit', damage);
+        return;
+    }
+}
+function biopsy(event) {
+    event.preventDefault();
+    var biopsy = document.getElementById('biopsy-y').checked;
+    form3.style.display = "none";
+    form4.style.display = "none";
+    if (biopsy == true) {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML = surgery[1];
+        form3.style.display = "block";
+        let x = document.querySelector('input[name=meta]:checked');
+        if (x) {
+            x.checked = false;
+        }
+        return;
+    } else {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML = surgery[2];
+        form4.style.display = "block";
+        let x = document.querySelector('input[name=damage]:checked');
+        if (x) {
+            x.checked = false;
+        }
+        return;
+    }
+}
+function meta(event) {
+    event.preventDefault();
+    var meta = document.getElementById('meta-y').checked;
+    if (meta == true) {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML = surgery[3];
+        return;
+    } else {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML = surgery[5];
+        return;
+    }
+}
+function damage(event) {    
+    event.preventDefault();
+    var damage = document.getElementById('damage-y').checked;
+    if (damage == true) {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML = surgery[3];
+        return;
+    } else {
+        conclusion.style.display = "inline-grid";
+        content.innerHTML = surgery[5];
+        return;
+    }
+}
+form1.addEventListener('submit', submitForm);
+form2.addEventListener('submit', biopsy);
+form3.addEventListener('submit', meta);
+form4.addEventListener('submit', damage);
